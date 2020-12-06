ddf09edd4ec479e820a3da0af1d14da10e5b3edf
appmonster007 <popme@pop-os.localdomain>
2020-12-06 17:14:56 +0530

    decrpyt method was moot

--- src/moduleMD5.java
+++ src/moduleMD5.java
@@ -3,8 +3,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigInteger;
 import java.security.DigestInputStream;
-import java.security.MessageDigest; 
-import java.security.NoSuchAlgorithmException; 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 // Java program to calculate MD5 hash value 
 public class moduleMD5 {
@@ -41,34 +42,17 @@ public class moduleMD5 {
 		} 
     }
     
-    public static String decryptMD5(String input) 
-	{ 
-		try { 
-
-            input = input.toLowerCase();
-			// Static getInstance method is called with hashing MD5 
-			MessageDigest md = MessageDigest.getInstance("MD5"); 
-
-			// digest() method is called to calculate message digest 
-			// of an input digest() return array of byte 
-			byte[] messageDigest = md.digest(input.getBytes()); 
-
-			// Convert byte array into signum representation 
-			BigInteger noBigInteger = new BigInteger(1, messageDigest); 
-
-			// Convert message digest into hex value 
-			String hashtext = noBigInteger.toString(16); 
-			while (hashtext.length() < 32) { 
-				hashtext = "0" + hashtext; 
-			} 
-			return hashtext; 
-		} 
-
-		// For specifying wrong message digest algorithms 
-		catch (NoSuchAlgorithmException e) { 
-			throw new RuntimeException(e); 
-		} 
-	}
+    private static byte[] getSalt() throws NoSuchAlgorithmException
+    {
+        //Always use a SecureRandom generator
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+        //Create array for salt
+        byte[] salt = new byte[16];
+        //Get a random salt
+        sr.nextBytes(salt);
+        //return salt
+        return salt;
+}
 
     private static byte[] checksum(String filePath) {
 
